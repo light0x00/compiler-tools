64420078acebe5091c1f54faef43840d08236ddc
light <light0x00@163.com>
2019-11-30 15:26:33 +0800

    chore:dev enviroment

--- test/test.ts
+++
@@ -1,8 +0,0 @@
-import "should";
-import { add } from "../src/index";
-
-describe("============LR Parser Test============", function () {
-	it(`is expected`, function () {
-		add(1, 2).should.eql(3);
-	});
-});
